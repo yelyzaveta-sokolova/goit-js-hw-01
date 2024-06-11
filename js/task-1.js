a589cc2d93c969e05682a4248723e3f823cdc58c
Yelyzaveta <yelyzavetasokolova71@gmail.com>
2024-06-12 00:47:28 +0200

    1/0

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const message =`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+    return message;   
+};
+
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
